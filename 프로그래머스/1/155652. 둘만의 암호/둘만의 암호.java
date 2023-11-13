36a95da62f2bf2fe14921d2029096a8ce6fbf3bf
Jung JiSung <jung48182@gmail.com>
2023-11-13 16:42:09 +0900

    [level 1] Title: 둘만의 암호, Time: 11.79 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -2,28 +2,22 @@ class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
         
-        // 50*10*20
+        boolean[] dp = new boolean[26];
+        for(int i=0; i<skip.length(); i++) {
+            dp[skip.charAt(i) - 'a'] = true;
+        }
+        
         for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            //z
+            char c = s.charAt(i);            
             
             int count = index;
             while(count > 0) {
                 char nc = (char) (c+1);
-                if(nc > 'z') nc = 'a'; //a
+                if(nc > 'z') nc = 'a';
                 
-                boolean canSkip = false;
-                for(int j=0; j<skip.length(); j++) {
-                    if(nc == skip.charAt(j)) {
-                        canSkip = true;
-                        break;
-                    }
-                }
-                
-                if(!canSkip) count--;    
+                if(!dp[nc - 'a']) count--;
                 c = nc;
             }
-            
             answer += c;
         }
         
