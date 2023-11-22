f0ff35ca0719d2225a8a25c61425290c03732e25
Jung JiSung <jung48182@gmail.com>
2023-11-22 15:24:24 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 12.16 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,14 +3,7 @@ import java.util.*;
 class Solution {
     public String solution(String s) {                
         int[] sequence = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
-        
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<sequence.length; i++) {
-            max = Math.max(max, sequence[i]);
-            min = Math.min(min, sequence[i]);
-        }
-        
-        return min + " " + max;
+        return Arrays.stream(sequence).min().getAsInt() + " " + Arrays.stream(sequence).max().getAsInt();
+            
     }
 }
