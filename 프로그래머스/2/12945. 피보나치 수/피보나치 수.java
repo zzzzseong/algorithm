2d983760d333c0ab90b9b876293549bf341cf8f8
Jung JiSung <jung48182@gmail.com>
2023-11-24 14:08:51 +0900

    [level 2] Title: 피보나치 수, Time: 2.79 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -3,14 +3,10 @@ class Solution {
         int[] sequence = new int[n+1];
         sequence[0] = 0;
         sequence[1] = 1;
-        fibonacci(sequence, 2, n);
-        return sequence[n] % 1_234_567;
-    }
-    
-    public void fibonacci(int[] sequence, int f, int n) {
-        if(f > n) return;
         
-        sequence[f] = (sequence[f-1] + sequence[f-2]) % 1_234_567;
-        fibonacci(sequence, f+1, n);
+        for(int i=2; i<=n; i++) {
+            sequence[i] = (sequence[i-1] + sequence[i-2]) % 1_234_567;
+        }
+        return sequence[n] % 1_234_567;
     }
 }
