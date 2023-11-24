2f88a4af6f1cdbc41fce84d413489c92e940940c
Jung JiSung <jung48182@gmail.com>
2023-11-24 14:19:33 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,13 +1,7 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        getDivisor(answer, brown, yellow);
-        return answer;
-    }
-    
-    public void getDivisor(int[] answer, int brown, int yellow) {
         double num = Math.sqrt(yellow);
-        
         for(int i=1; i<=num; i++) {
             if(yellow % i == 0) {
                 int width = yellow/i;
@@ -16,8 +10,11 @@ class Solution {
                 if((width + height)*2 + 4 == brown) {
                     answer[0] = width+2;
                     answer[1] = height+2;
+                    break;
                 }
             }
         }
+        
+        return answer;
     }
 }
