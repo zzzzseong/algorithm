727eb4095b860c26a669eabff2e3147f095c544b
Jung JiSung <jung48182@gmail.com>
2023-11-30 09:13:17 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int n) {
+        if(n == 1) return 1;
+        
+        long[] dp = new long[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1_234_567;
+        }
+        
+        return dp[n] % 1_234_567;
+    }
+}
