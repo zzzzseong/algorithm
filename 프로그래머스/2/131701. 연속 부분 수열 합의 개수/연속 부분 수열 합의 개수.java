fbc985a53b5e75a3ff91517764abba833b4c6630
Jung JiSung <jung48182@gmail.com>
2023-12-01 14:45:34 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 92.02 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,21 +3,30 @@ import java.util.*;
 class Solution {
     private Set<Integer> set = new HashSet<>();
     private int[] circle;
+    private int[][] dp;
     
     public void init(int[] elements) {
-        circle = new int[elements.length*2];
-        for(int i=0; i<elements.length; i++) {
+        int N = elements.length;
+        circle = new int[N*2];
+        for(int i=0; i<N; i++) {
             circle[i] = elements[i];
             circle[elements.length + i] = elements[i];
+        }
+        
+        dp = new int[N][N+1];
+        for(int i=0; i<N; i++) {
+            dp[i][0] = elements[i];
+            set.add(dp[i][0]);
         }
     }
     
     public int solution(int[] elements) {
         init(elements);
         
-        for(int i=0; i<elements.length; i++) {            
-            for(int j=0; j<elements.length; j++) {
-                set.add(Arrays.stream(circle, i, i+j).sum());
+        for(int i=1; i<elements.length; i++) {
+            for(int j=0; j<elements.length; j++) {                
+                dp[j][i] = dp[j][i-1] + circle[i+j];
+                set.add(dp[j][i]);
             }
         }
         
