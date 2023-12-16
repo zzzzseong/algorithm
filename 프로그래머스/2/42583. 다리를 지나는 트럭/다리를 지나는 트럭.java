aeab10e66b3fa77ac2968fb51407fa333fc4b870
Jung JiSung <jung48182@gmail.com>
2023-12-16 14:40:32 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.23 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    private Queue<Truck> bridge = new LinkedList<>();
+    
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int time = 1;
+        int idx = 0;
+        
+        bridge.offer(new Truck(time++, truck_weights[idx]));
+        int total = truck_weights[idx++];
+        
+        while(!bridge.isEmpty()) {
+            if((time-bridge.peek().getEnter()) == bridge_length) {
+                //가장 앞 트럭이 나올 수 있다면
+                total -= bridge.poll().getWeight();
+            }
+            
+            if(idx < truck_weights.length && total + truck_weights[idx] <= weight) {
+                //트럭이 진입할 수 있다면
+                bridge.offer(new Truck(time, truck_weights[idx]));
+                total += truck_weights[idx++];
+            }
+            
+            time++;
+        }
+        
+        return time-1;
+    }
+}
+
+class Truck {
+    private int enter;
+    private int weight;
+    
+    public Truck(int enter, int weight) {
+        this.enter = enter;
+        this.weight = weight;
+    }
+    
+    public int getEnter() {
+        return enter;
+    }
+    public int getWeight() {
+        return weight;
+    }
+}
