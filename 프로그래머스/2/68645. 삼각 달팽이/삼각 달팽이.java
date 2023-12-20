cc8348912a80f4e15dc03bdb5194f64d29412e85
Jung JiSung <jung48182@gmail.com>
2023-12-20 16:20:12 +0900

    [level 2] Title: 삼각 달팽이, Time: 20.06 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] solution(int n) {
+        int[][] map = new int[n][n];
+        
+        int x = -1;
+        int y = 0;
+        int num = 1;
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                if(i%3 == 0) {  
+                    x++;
+                }
+                if(i%3 == 1) {
+                    y++;
+                }
+                if(i%3 == 2) {
+                    x--;
+                    y--;
+                }
+                
+                map[x][y] = num++;
+            }
+        }
+        
+        int idx = 0;
+        int[] answer = new int[n*(n+1)/2];
+        for(int i=0; i<map.length; i++) {
+            for(int j=0; j<map[i].length; j++) {
+                if(map[i][j] != 0) answer[idx++] = map[i][j];
+                else break;
+            }
+        }
+        
+        return answer;
+    }
+}
