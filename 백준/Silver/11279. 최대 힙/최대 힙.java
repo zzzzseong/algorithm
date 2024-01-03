4fb0aba28e05ce0d676df58a31c70eabbde3a4fe
Jung JiSung <jung48182@gmail.com>
2024-01-03 17:25:24 +0900

    [Silver II] Title: 최대 힙, Time: 344 ms, Memory: 26524 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if(x == 0) {
+                if(pq.isEmpty()) sb.append(0).append("\n");
+                else sb.append(pq.poll()).append("\n");
+            } else {
+                pq.offer(x);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
