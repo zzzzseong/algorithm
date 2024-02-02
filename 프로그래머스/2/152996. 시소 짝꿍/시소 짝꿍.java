cb4164fce65a208934583ad5a412bc81d70635e2
Jung JiSung <jung48182@gmail.com>
2024-02-02 10:49:48 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.42 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -10,11 +10,13 @@ class Solution {
     		double b = (i*2.0)/3.0;
     		double c = (i*1.0)/2.0;
     		double d = (i*3.0)/4.0;
+            
     		if(map.containsKey(a)) answer += map.get(a);
     		if(map.containsKey(b)) answer += map.get(b);
     		if(map.containsKey(c)) answer += map.get(c);
     		if(map.containsKey(d)) answer += map.get(d);
-    		map.put((i*1.0), map.getOrDefault((i*1.0), 0)+1);
+            
+    		map.put((i*1.0), map.getOrDefault(i*1.0, 0)+1);
         }
         
         return answer;
