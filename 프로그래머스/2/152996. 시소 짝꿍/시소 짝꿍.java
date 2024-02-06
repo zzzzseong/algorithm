9f57b566456ddfe0286895ff3702eb2866a9f0aa
Jung JiSung <jung48182@gmail.com>
2024-02-06 22:37:40 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.18 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,24 +1,41 @@
 import java.util.*;
 
 class Solution {
+    private Map<Integer, Long> wm = new HashMap<>();
+    private Map<Integer, Long> map = new HashMap<>();
+    
+    private long answer = 0;
+    
+    private void init(int[] weights) {
+        for(int w : weights) {
+            if(wm.get(w) == null) {
+                wm.put(w, 1L);
+                continue;
+            }
+            
+            answer += wm.get(w);
+            wm.put(w, wm.get(w)+1);
+        }
+    }
+    
     public long solution(int[] weights) {
-    	long answer = 0;
-        Arrays.sort(weights);
-        Map<Double, Integer> map = new HashMap<>();
-        for(int i : weights) {
-    		double a = i*1.0;
-    		double b = (i*2.0)/3.0;
-    		double c = (i*1.0)/2.0;
-    		double d = (i*3.0)/4.0;
-            
-    		if(map.containsKey(a)) answer += map.get(a);
-    		if(map.containsKey(b)) answer += map.get(b);
-    		if(map.containsKey(c)) answer += map.get(c);
-    		if(map.containsKey(d)) answer += map.get(d);
-            
-    		map.put((i*1.0), map.getOrDefault(i*1.0, 0)+1);
+        init(weights);
+        
+        for(Integer key : wm.keySet()) {
+            setMap(key, key*2);
+            setMap(key, key*3);
+            setMap(key, key*4);
         }
         
         return answer;
+    }
+    
+    public void setMap(int key, int mKey) {
+        if(map.get(mKey) == null) {
+            map.put(mKey, wm.get(key));
+        } else {
+            answer += map.get(mKey)*wm.get(key);
+            map.put(mKey, map.get(mKey) + wm.get(key));
+        }
     }
 }
