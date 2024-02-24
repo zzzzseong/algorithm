86171dd3a5aa87e7563a39cffca81768afc43eeb
Jung JiSung <jung48182@gmail.com>
2024-02-24 16:49:13 +0900

    [level 0] Title: 배열 만들기 5, Time: 5.80 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<intStrs.length; i++) {
+            int num = Integer.parseInt(intStrs[i].substring(s, s+l));
+            
+            if(num > k) list.add(num);
+        }
+        
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
