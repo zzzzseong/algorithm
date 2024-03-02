3c303772d15cba77b8b2527fe17c52f4df2ddbd6
Jung JiSung <jung48182@gmail.com>
2024-03-02 14:17:19 +0900

    [level 0] Title: 리스트 자르기, Time: 2.23 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {        
+        List<Integer> list = new ArrayList<>();
+        
+        switch(n) {
+            case 1:
+                for(int i=0; i<=slicer[1]; i++) {
+                    list.add(num_list[i]);
+                }
+                break;
+            case 2:
+                for(int i=slicer[0]; i<num_list.length; i++) {
+                    list.add(num_list[i]);
+                }
+                break;
+            case 3:
+                for(int i=slicer[0]; i<=slicer[1]; i++) {
+                    list.add(num_list[i]);
+                }
+                break;
+            default: 
+                for(int i=slicer[0]; i<=slicer[1]; i+=slicer[2]) {
+                    list.add(num_list[i]);
+                }
+                break;
+        }
+        
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
