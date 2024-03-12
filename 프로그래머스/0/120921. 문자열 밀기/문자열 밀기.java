9df0d8ee6597d5dc2f4372d3313ae10804fef356
Jung JiSung <jung48182@gmail.com>
2024-03-13 01:25:33 +0900

    [level 0] Title: 문자열 밀기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,14 +1,5 @@
 class Solution {
     public int solution(String A, String B) {
-        
-        int len = A.length();
-        
-        for(int i=len; i>=0; i--) {
-            String str = A.substring(i, len) + A.substring(0, i);
-                        
-            if(B.equals(str)) return len-i;
-        }
-        
-        return -1;
+        return B.repeat(2).indexOf(A);
     }
 }
