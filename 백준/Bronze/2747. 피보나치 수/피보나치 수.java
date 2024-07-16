eb9ccafd44b40a113237f198011dc25323a7ea59
Jung JiSung <jung48182@gmail.com>
2024-07-16 21:42:56 +0900

    [Bronze II] Title: 피보나치 수, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static long[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        dp = new long[46];
+        dp[1] = 1;
+        dp[2] = 1;
+
+        System.out.println(fibo(n));
+    }
+
+    public static long fibo(int n) {
+        if(dp[n] != 0) return dp[n];
+
+        dp[n] = fibo(n-1) + fibo(n-2);
+        return dp[n];
+    }
+}
