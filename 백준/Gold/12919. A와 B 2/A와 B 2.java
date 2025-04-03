18dba1e57236ccc33850371fc32add886adf211e
Jung JiSung <jung48182@gmail.com>
2025-04-03 21:47:27 +0900

    [Gold V] Title: A와 B 2, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -8,15 +8,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 1 <= word1의 길이 <= 49
-        // 2 <= word2의 길이 <= 50
-        // word1의 길이 < word2의 길이
-
-        // operation1. 문자열 뒤에 A를 추가
-        // operation2. 문자열 뒤에 B를 추가하고 문자열을 뒤집기
-
-        //word1을 word2로 만들 수 있다면 1을 출력, 그렇지 않다면 0을 출력
-
         String word1 = br.readLine();
         String word2  = br.readLine();
 
@@ -25,18 +16,20 @@ public class Main {
         System.out.println(answer);
     }
 
+    /**
+     * @param word 현재 문자열
+     * @param target 목표 문자열
+     * @param wl 현재 문자열 길이
+     * @param tl 목표 문자열 길이
+     * */
     public static void traverse(String word, String target, int wl, int tl) {
         if(word.equals(target)) answer = 1;
         if(wl < tl || answer == 1) return;
 
-        char first = word.charAt(0);
-        char last = word.charAt(wl-1);
+        char head = word.charAt(0);
+        char tail = word.charAt(wl-1);
 
-        if(first == 'B') {
-            traverse(new StringBuilder(word.substring(1)).reverse().toString(), target, wl-1, tl);
-        }
-        if(last == 'A') {
-            traverse(word.substring(0, wl-1), target, wl-1, tl);
-        }
+        if(head == 'B') traverse(new StringBuilder(word.substring(1)).reverse().toString(), target, wl-1, tl);
+        if(tail == 'A') traverse(word.substring(0, wl-1), target, wl-1, tl);
     }
 }
